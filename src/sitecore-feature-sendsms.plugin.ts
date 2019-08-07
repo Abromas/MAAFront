18f09e5118eb86866206b58c28f35968ea462239
Aivaras Abromas <Aivaras.Abromas@macaw.nl>
2019-08-07 22:04:55 +0300

    first commit

---
+++ src/sitecore-feature-sendsms.plugin.ts
@@ -0,0 +1,18 @@
+﻿import { Plugin } from '@sitecore/ma-core';
+import { SendSmsActivity } from './sitecore-feature-sendsms/SendSms-activity';
+import { SendSmsModuleNgFactory } from '../codegen/sitecore-feature-sendsms/SendSms-module.ngfactory';
+import { ReadonlyEditorComponent } from '../codegen/sitecore-feature-sendsms/editor/readonly-editor.component';
+
+// Use the @Plugin decorator to define all the activities the module contains.
+@Plugin({
+    activityDefinitions: [
+        {
+            // The ID must match the ID of the activity type description definition item in the CMS.
+            id: '19BBDCE4-F48C-4D63-A7BB-4A1FDDEEA997'.toLowerCase(), 
+            activity: SendSmsActivity,
+            editorComponenet: ReadonlyEditorComponent,
+            editorModuleFactory: SendSmsModuleNgFactory
+        }
+    ]
+})
+export default class SendSmsPlugin {}
---
+++ src/sitecore-feature-sendsms/sendsms-activity.ts
@@ -0,0 +1,25 @@
+﻿import { SingleItem} from '@sitecore/ma-core';
+
+export class SendSmsActivity extends SingleItem {
+	
+    getVisual(): string {
+        const subTitle = 'Send SMS';
+        const cssClass = this.isDefined ? '' : 'undefined';
+        
+        return `
+            <div class="viewport-readonly-editor marketing-action ${cssClass}">
+                <span class="icon">
+                    <img src="/~/icon/People/32x32/mda.png" />
+                </span>
+                <p class="text with-subtitle" title="Send SMS">
+                    Send SMS
+                    <small class="subtitle" title="${subTitle}">${subTitle}</small>
+                </p>
+            </div>
+        `;
+    }
+
+    get isDefined(): boolean {
+        return true;
+    }
+}
